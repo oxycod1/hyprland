d9b46cfbfe27bb664cadf145aee1a4b3b8dbba23
oxycod1 <161760315+oxycod1@users.noreply.github.com>
2024-02-29 23:40:03 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+sudo pacman -S firefox hyprland waybar hyprpaper thunar ttf-jetbrains-mono polkit-gnome grim slurp wl-clipboard arc-gtk-theme kitty wofi eog mpv papirus-icon-theme gvfs
+
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ..
+
+yay -S hyprpicker vimix-cursors
+
+gsettings set org.gnome.desktop.interface gtk-theme "Arc-Lighter"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Light"
+gsettings set org.gnome.desktop.interface cursor-theme "Vimix-cursors"
+gsettings set org.gnome.desktop.interface font-name "JetBrains Mono 9"
+gsettings set org.gnome.desktop.wm.preferences theme "Arc-Lighter"
+gsettings set org.gnome.desktop.wm.preferences button-layout :
+
+mkdir ~/.fonts
+mkdir ~/.fonts/TTF
+cp fonts/* ~/.fonts/TTF
+
+cp -r fontconfig ~/.config
+cp -r hypr ~/.config
+cp -r kitty ~/.config
+cp -r waybar ~/.config
+cp -r wofi ~/.config
---
+++ waybar/theme.sh
@@ -0,0 +1,35 @@
+THEME=light
+
+if [ $THEME == "light" ]; then
+  sed -i "1s/.*/THEME=dark/" ~/.config/waybar/theme.sh
+	sed -i -e "s/Vimix-cursors/Vimix-white-cursors/g" ~/.config/hypr/hyprland.conf
+	sed -i -e "s/dark.jpg/light.jpg/g" ~/.config/hypr/hyprpaper.conf
+	sed -i -e "s/include light.conf/include dark.conf/g" ~/.config/kitty/kitty.conf
+
+	gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
+	gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+	gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+	gsettings set org.gnome.desktop.interface cursor-theme "Vimix-white-cursors"
+	gsettings set org.gnome.desktop.wm.preferences theme "Arc-Dark"
+
+	hyprctl setcursor "Vimix-white-cursors" 24
+
+	killall hyprpaper
+	exec hyprpaper
+elif [ $THEME == "dark" ]; then
+	sed -i "1s/.*/THEME=light/" ~/.config/waybar/theme.sh
+	sed -i -e "s/Vimix-white-cursors/Vimix-cursors/g" ~/.config/hypr/hyprland.conf
+	sed -i -e "s/light.jpg/dark.jpg/g" ~/.config/hypr/hyprpaper.conf
+	sed -i -e "s/include dark.conf/include light.conf/g" ~/.config/kitty/kitty.conf
+
+	gsettings set org.gnome.desktop.interface gtk-theme "Arc-Lighter"
+	gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+	gsettings set org.gnome.desktop.interface icon-theme "Papirus-Light"
+	gsettings set org.gnome.desktop.interface cursor-theme "Vimix-cursors"
+	gsettings set org.gnome.desktop.wm.preferences theme "Arc-Lighter"
+
+	hyprctl setcursor "Vimix-cursors" 24
+
+	killall hyprpaper
+	exec hyprpaper
+fi
